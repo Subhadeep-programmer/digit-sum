d0de87961f3b7430c46489c8a316434146984876
Subhadeep Banerjee <63862729+Subhadeep-programmer@users.noreply.github.com>
2020-04-29 12:26:44 +0530

    digit-sum.c
    this prints the summation of 5 digits

---
+++ digit-sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+addition();
+
+void main()
+{
+ addition();
+}
+
+addition()
+{
+ int a=0,b=0,c=0,d=0,e=0;
+ printf("Enter the 5 numbers:");
+ scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+ printf("The sum is : %d",a+b+c+d+e);
+}
